ee3ef5dac1e64dd712451e7d9146a67b257096aa
Emmanuel Rojas <emmanuel.rojas@nodoelectronico.com>
2018-12-12 16:05:45 -0600

    Site redesigned

--- solecys-homepage/js/main.js
+++ solecys-homepage/js/main.js
@@ -1,5 +1,28 @@
+ScrollOut();
+
 ScrollOut({
   threshold: 1,
   once: true
 });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let navbar = document.querySelector('#navbar');
+
+  document.addEventListener("scroll", (event) => {
+    if(document.documentElement.scrollTop > 200 || document.scrollingElement.scrollTop > 200) {
+      navbar.setAttribute("class","blue-grey darken-4 solid");
+    }
+    else {
+      navbar.setAttribute("class","transparent z-depth-0");
+    }
+  });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, options);
+  });
+
+
+})
 
