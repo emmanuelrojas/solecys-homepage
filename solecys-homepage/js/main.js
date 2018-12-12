fd54e1c1e9be2cc34b5f24ab22dbec7b6823c3aa
Emmanuel Rojas <emmanuel.rojas@nodoelectronico.com>
2018-12-12 13:01:10 -0600

    Before redesign

--- solecys-homepage/js/main.js
+++ solecys-homepage/js/main.js
@@ -1,16 +1,5 @@
-let controller = new ScrollMagic.Controller();
-
-let containerScene = new ScrollMagic.Scene({
-  triggerElement: '#container',
-  duration: 300
+ScrollOut({
+  threshold: 1,
+  once: true
 });
-
-let blockTween = new TweenMax.to('#dark-layer', 1.5, {
-  opacity: '0'
-});
-
-containerScene.setTween(blockTween)
-containerScene.setPin('#block')
-.addIndicators()
-.addTo(controller);
 
